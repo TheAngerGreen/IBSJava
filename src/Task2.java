40f25b959fd146d005efd7485721bea08cd25874
Alexandr Yakovlev <156159151+TheAngerGreen@users.noreply.github.com>
2025-03-30 23:00:05 +0700

    work

--- src/Task2.java
+++ src/Task2.java
@@ -1,7 +1,7 @@
 public class Task2 {
     public static void main(String[] args) {
-        double value1 = 12.3;
-        byte value2 = 42;
+        double value1 = 3827.3423423;
+        byte value2 = 127;
         System.out.println("Значение переменной типа с плавающей точкой: " + value1);
         System.out.println("Значение переменной целочисленного типа: " + value2);
     }
